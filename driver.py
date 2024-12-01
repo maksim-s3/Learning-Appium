db2245d2f65c86a55497b6f06c7e72d29ac33a8b
m.startsev <m.startsev@tquality.ru>
2024-12-01 21:35:21 +0300

    Add appium driver

---
+++ driver.py
@@ -0,0 +1,20 @@
+from appium import webdriver
+from appium.options.android import UiAutomator2Options
+
+capabilities = dict(
+    platformName='Android',
+    automationName='uiautomator2',
+)
+
+appium_server_url = 'http://localhost:4723'
+
+
+class AppiumDriver:
+    __driver = None
+
+    @classmethod
+    def get_driver(cls):
+        if not cls.__driver:
+            cls.__driver = webdriver.Remote(
+                appium_server_url, options=UiAutomator2Options().load_capabilities(capabilities))
+        return cls.__driver
